72d430161f6ecccfdb25885b95aabd7735d777c8
Abhishek Vadodariya <37392803+abhishekvadodariya@users.noreply.github.com>
2023-11-24 11:02:20 +0000

    working on design

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
           hintStyle: TextStyle(
             fontWeight: FontWeight.bold,
             fontSize: 16,
+            color: Colors.black
           ),
           prefixIconColor: Color.fromRGBO(119, 119, 119, 1),
         ),
